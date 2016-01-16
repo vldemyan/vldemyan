84438495c5bbc2830a22ab66f052778354365ca8
Volodymyr Demyanenko <Volodymyr.Demyanenko@portaone.com>
2016-01-16 20:50:34 +0200

    uncomment lines

--- equal_is_not_equal.pl
+++ equal_is_not_equal.pl
@@ -4,14 +4,17 @@ use strict;
 use Data::Dumper;
 
 my $a = [
-#    1.9999999999999999,
+    1.9999999999999999,
     1.999999999999999,
-#    1.99999999999999,
+    1.99999999999999,
 ];
 my $b = 2;
 
 foreach my $i ( 0..$#$a ) {
+    my $a->[$i] = sprintf ("%.5f", $a->[$i]);
     print "$a->[$i] and $b are equal\n" if ($a->[$i] == $b);
     print "$a->[$i] is greater than $b\n" if ($a->[$i] > $b);
     print "$a->[$i] is lower than $b\n" if ($a->[$i] < $b);
 }
+
+print (sprintf ("%.5f", $a->[0]));
